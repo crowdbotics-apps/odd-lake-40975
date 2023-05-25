3ea7d4cfeaa22ec4fead25ec516cfde5b6bfe549
Crowdbotics <team@crowdbotics.com>
2023-05-25 01:36:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "odd-lake-40975",
+  url: "https://odd-lake-40975.botics.co",
+  api: "https://odd-lake-40975.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
